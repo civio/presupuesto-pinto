47c93e659204a07328af62621c538ed51c022393
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-03-18 19:30:21 +0100

    Update payments loader

--- loaders/pinto_payments_loader.py
+++ loaders/pinto_payments_loader.py
@@ -2,5 +2,17 @@
 from budget_app.loaders import PaymentsLoader
 
 class PintoPaymentsLoader(PaymentsLoader):
-    # The default class is fine for us
-    pass
+
+    # Parse an input line into fields
+    def parse_item(self, budget, line):
+        return {
+            'area': line[3].strip(),
+            'programme': line[2].strip(),
+            'fc_code': None,  # We don't try (yet) to have foreign keys to existing records
+            'ec_code': None,
+            'date': line[0].strip(),
+            'contract_type': None,
+            'payee': self._titlecase(line[6].strip()),
+            'description': self._spanish_titlecase(line[7].strip()),
+            'amount': self._read_english_number(line[1])
+        }
