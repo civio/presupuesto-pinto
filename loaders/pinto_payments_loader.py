e98b87ab86223dca71fded6c6bbfbb3f53d17ed5
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-03-23 19:24:20 +0100

    Update payments loader

--- loaders/pinto_payments_loader.py
+++ loaders/pinto_payments_loader.py
@@ -1,13 +1,24 @@
 # -*- coding: UTF-8 -*-
 from budget_app.loaders import PaymentsLoader
+from budget_app.models import Budget
 
 class PintoPaymentsLoader(PaymentsLoader):
 
     # Parse an input line into fields
     def parse_item(self, budget, line):
+
+        programme_id = line[3].strip()[:2]
+
+        # Convert 11 to 01 for Deuda Pública
+        if programme_id == '11':
+            programme_id = '01'
+
+        # But what we want as area is the programme description
+        programme = Budget.objects.get_all_descriptions(budget.entity)['functional'][programme_id]
+
         return {
-            'area': line[3].strip(),
-            'programme': line[2].strip(),
+            'area': programme,
+            'programme': None,
             'fc_code': None,  # We don't try (yet) to have foreign keys to existing records
             'ec_code': None,
             'date': line[0].strip(),
