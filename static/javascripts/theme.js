3f83002477ced2346891dbe1155ad0ab77e9923d
David Cabo <david.cabo@gmail.com>
2018-03-02 16:00:15 +0100

    Add note on budget extensions civio/presupuesto-management#631

--- static/javascripts/theme.js
+++ static/javascripts/theme.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 
     $('.data-controllers .layout-slider .slider .slider-tick-label').each(function(){
       var val = $(this).html();
-      if (val === '2017' || val === '2018'){
+      if (val === '2016' || val === '2017' || val === '2018'){
         $(this).html(val + '<br/><small><i> ('+ str2017[ $('html').attr('lang') ] +')</i></small>');
       }
     });
