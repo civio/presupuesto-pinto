bd45f3e923921ddcbb4b96db7e8e6ba040eea1ec
David Cabo <david.cabo@gmail.com>
2018-04-01 02:43:01 +0200

    Remove unused fields from Payment model civio/presupuesto-management#667

--- loaders/pinto_payments_loader.py
+++ loaders/pinto_payments_loader.py
@@ -24,7 +24,6 @@ class PintoPaymentsLoader(PaymentsLoader):
             'fc_code': None,  # We don't try (yet) to have foreign keys to existing records
             'ec_code': None,
             'date': line[0].strip(),
-            'contract_type': None,
             'payee': self._titlecase(line[6].strip()),
             'description': self._spanish_titlecase(line[7].strip()),
             'amount': self._read_english_number(line[1])
