024eb71a08edcac87c9718b91c099e71700fbca1
David Cabo <david.cabo@gmail.com>
2021-12-01 16:07:37 +0100

    Add payment data for 2019 and 2020 civio/presupuesto-management#1139

--- loaders/pinto_payments_loader.py
+++ loaders/pinto_payments_loader.py
@@ -7,6 +7,8 @@ import re
 payments_mapping = {
     'default': {'fc_code': 3, 'date': 0, 'payee': 6, 'description': 7, 'amount': 1},
     '2018': {'fc_code': 3, 'date': 1, 'payee': 6, 'description': 7, 'amount': 4},
+    '2019': {'fc_code': 3, 'date': 1, 'payee': 6, 'description': 7, 'amount': 4},
+    '2020': {'fc_code': 3, 'date': 1, 'payee': 6, 'description': 7, 'amount': 4},
 }
 
 
@@ -35,7 +37,7 @@ class PintoPaymentsLoader(PaymentsLoader):
 
         # In 2018 we don't get the functional code separately, so we use
         # the full budget heading
-        if budget.year == 2018 and fc_code:
+        if budget.year in [2018, 2019, 2020] and fc_code:
             fc_code = fc_code.split()[2]
 
         # We got some 2- digit codes or even empty ones, so we normalize them
